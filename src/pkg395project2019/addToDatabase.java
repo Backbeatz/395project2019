84cf80df9f63180f633b14ffdc9c3a303cd668e4
Konecnym <michael.j.konecny@gmail.com>
2018-10-23 12:16:37 -0600

    update of addToDatabase;

--- src/pkg395project2019/addToDatabase.java
+++ src/pkg395project2019/addToDatabase.java
@@ -1,4 +1,4 @@
-package pkg395project2019;
+package project395;
 
 /**
  *
@@ -11,30 +11,30 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class addToDatabase {
-        public void insertNewContractor(int PersonID, String Username, String Password, int Autherization) {
+        public void insertNewContractor(int PersonID, String Username, String Password, int Authorization) {
         
-            String sql = "INSERT INTO Contractor(PersonID, Username, Password, Autherization) VALUES(?,?,?,?)";
+            String sql = "INSERT INTO Contractor(PersonID, Username, Password, Authorization) VALUES(?,?,?,?)";
             
             try (Connection conn = dbConnect.connect();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
                 pstmt.setInt(1, PersonID);
                 pstmt.setString(2, Username);
                 pstmt.setString(3, Password);
-                pstmt.setInt(2, Autherization);
+                pstmt.setInt(4, Authorization);
                 pstmt.executeUpdate();
                 System.out.println("Success!"); //Test Line Please Delete after confirmation
                 
             } catch (SQLException e) {
-                System.out.println(e.getMessage());
-        }
+                System.out.println("Failure!"); //Test Line Please Delete after confirmation
+                System.out.println(e.getMessage());        }
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         addToDatabase test = new addToDatabase();
-        test.insertNewContractor(1,"Mike","Password",1);
-                
+        test.insertNewContractor(2,"Mike2","Password2",1);
+        
     }
-    
 }
+
