81be1aea19add23b013f53f586873d02a46f066b
Backbeatz <connor.pochylko@gmail.com>
2018-11-14 19:38:35 -0700

    Update: -Added a personal id generator -Add contractor can now add values to db

--- src/java/pkg395project2019/bean1.java
+++ src/java/pkg395project2019/bean1.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+ 
 
 @ManagedBean(name="bean1")
 @RequestScoped
@@ -45,7 +46,7 @@ public class bean1 {
     private String createPass;
     private String firstName;
     private String lastName;
-    private String job;
+    private String job; //profession
     private String city;
     private String province;
     private String postalCode;
@@ -60,18 +61,15 @@ public class bean1 {
     public String getMonth(){
         return month;
     }
-    
     public String getYear(){
         return year;
     }
-    
     public String getHours(){
         return hours;
     }
     public void setHours(String hours){
         this.hours = hours;
     }
-
     //Current user
     public String getCurName(){
         return currentName;
@@ -96,22 +94,19 @@ public class bean1 {
     }
     public void setCurComp(String company){
         this.currentComp = company;
-    }
-    
+    }    
     public String getPass(){
         return pass;
     }
     public void setPass(String pass){
         this.pass = pass;
     }
-
     public String getNewPass(){
         return newPass;
     }
     public void setNewPass(String newPass){
         this.newPass = newPass;
-    }
-    
+    }    
     public String getUsername(){
         return username;
     }
@@ -124,78 +119,67 @@ public class bean1 {
     }
     public void setJob(String job){
         this.job = job;
-    }
-    
+    }    
     public String getDob(){
         return dob;
     }
     public void setDob(String dob){
         this.dob = dob;
-    }
-    
+    }    
     public String getCity(){
         return city;
     }
     public void setCity(String city){
         this.city = city;
-    }
-    
+    }    
     public String getSex(){
         return sex;
     }
     public void setSex(String sex){
         this.sex = sex;
-    }
-    
+    }   
     public String getCreateUser(){
         return createUser;
     }
     public void setCreateUser(String createUser){
         this.createUser = createUser;
-    }
-    
+    }    
     public String getCreatePass(){
         return createPass;
     }
     public void setCreatePass(String createPass){
         this.createPass = createPass;
-    }
-    
+    }    
     public String getProvince(){
         return province;
     }
     public void setProvince(String province){
         this.province = province;
-    }
-    
+    }    
     public String getPostalCode(){
         return postalCode;
     }
     public void setPostalCode(String postalCode){
         this.postalCode = postalCode;
-    }
-    
+    }    
     public String getEmail(){
         return email;
     }
     public void setEmail(String email){
         this.email = email;
-    }
-    
+    }    
     public String getPhone(){
         return phone;
     }
     public void setPhone(String phone){
         this.phone = phone;
-    }
-    
+    }    
     public String getFirstName(){
         return firstName;
     }
     public void setFirstName(String firstName){
         this.firstName = firstName;
-    }
-    
+    }    
     public String getLastName(){
         return lastName;
     }
@@ -291,11 +275,57 @@ public class bean1 {
         }
         
     }*/
-    
+    /**
+     * 
+     * @return webpage
+     */
     public String addContractor(){
-        //verify every field
-        return "contractor_added";
+        
+        //load
+        String a = getFirstName();
+        
+        //Query
+        query qConIn = new query();
+        Object[] infoNC = new Object[8];
+        infoNC[0]= giveNewID(1, 100); //person id
+        infoNC[1]=getFirstName(); //Fname
+        infoNC[2]=getLastName(); //Lname
+        infoNC[3]=getPhone(); //phone
+        infoNC[4]=getEmail();//email
+        infoNC[5]=getCreateUser(); //uname 
+        infoNC[6]=getCreatePass(); //pass
+        infoNC[7]=2; //Auth Hard code as 2 for now
+        // Waiting on db additions of sex and job
+            
+            
+            if(!qConIn.insert(1, infoNC)){
+                return "hours_reset";
+            } 
+        return "hours";
     }
+    /**
+     * This generates our person ID
+     * -Connor
+     * @return 
+     */
+    public int giveNewID(int min, int max){
+        //For each number in range check if in database
+        for(int i=min;i<max;i++){
+            query sample2 = new query();
+            String testQuery2;
+            Object[] testInfo2 = new Object[8];
+            testInfo2[0]=i;
+            testQuery2=sample2.selectWhere(1, testInfo2, testInfo2);
+            if(!sample2.selectQueryFromDb()){
+               return i;
+           }
+        }
+        
+        return 0;
+    }
+    
+    
+    
    /**
     * Sets our current user's info as the log in
     * @param username
@@ -309,11 +339,11 @@ public class bean1 {
         Object[] testInfo2 = new Object[8];
         testInfo2[5]=username;
         testInfo2[6]=password;
-        
+        //This is where we're waiting for our
         String Name = qConIn.selectWhere(1, testInfo2, testInfo2);
         
         //set our values ussing setters
-        setCurName(Name);
+        setCurName("Name");
         setCurEmail("Email");
         setCurPhone("Phone");
         setCurComp("Job");
@@ -360,5 +390,5 @@ public class bean1 {
         } catch (SQLException e){
             //Exception clause
             attempts += 1;
-            return "main_website_reset";
+            return "main_websi te_reset";
         }*/
