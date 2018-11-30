53f4555081ec6922bc810fa32100c965a90d8c78
Backbeatz <connor.pochylko@gmail.com>
2018-11-29 17:54:43 -0700

    Update 5: Change to bean 1

--- src/java/pkg395project2019/dbConnect.java
+++ src/java/pkg395project2019/dbConnect.java
@@ -20,7 +20,7 @@ public class dbConnect {
                     System.err.println("Could not init JDBC driver - driver not found");
                 }
             // db parameters Use Absolute Pathing name for individual PC
-            String url = "jdbc:sqlite:D:\\MikeK\\Documents\\GitHub\\395project2019\\contractDB.db";
+            String url = "jdbc:sqlite:C:\\Users\\Owner\\Documents\\NetBeansProjects\\395project2019\\contractDB.db";
             //String url = "jdbc:sqlite:contractDB.db:c:/Users/Owner/Documents/NetBeansProjects/395project2019"; 
             // create a connection to the database
             conn = DriverManager.getConnection(url);
