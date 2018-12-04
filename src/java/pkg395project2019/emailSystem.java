6b02520f8097c822e506f74858f53a1aeed83c5e
Backbeatz <connor.pochylko@gmail.com>
2018-12-04 00:15:11 -0700

    Update 8: bug fixes

--- src/java/pkg395project2019/emailSystem.java
+++ src/java/pkg395project2019/emailSystem.java
@@ -7,6 +7,7 @@
 package pkg395project2019;
 
 
+
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -19,40 +20,52 @@ import javax.mail.internet.MimeMessage;
 
 public class emailSystem {
 
-	public static void main(String[] args) {
-
+    /**
+     * Send email to target
+     * @param targetEmail target email
+     * @param msg1 message title
+     * @param msg2 message text
+     * @return
+     */
+    public boolean sendUMsg(String targetEmail, String msg1, String msg2 ) {
+                System.out.print("Step1" + targetEmail + msg1 + msg2);
 		final String username = "group395cmptemail@gmail.com";
 		final String password = "RunningOutOfTime1";
-
+                System.out.print("Step2");
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
-
+                System.out.print("Step3");
 		Session session = Session.getInstance(props,
 		  new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		  });
-
+                System.out.print("Step4");
 		try {
 
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("group395cmptemail@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse("jamia@mymacewan.ca")); //change this
-			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler,"
-				+ "\n\n No spam to my email, please!");
-
+			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(targetEmail)); 
+			message.setSubject(msg1);
+			message.setText(msg2);
+                        System.out.print("Step5.1");
 			Transport.send(message);
-
-			System.out.println("Done");
+                        System.out.print("Step here?");
+                        
 
 		} catch (MessagingException e) {
-			throw new RuntimeException(e);
+                        System.out.print("Step5.2");
+                        return false;
 		}
-	}
-}
+                System.out.print("Step6");
+                return true;
+    }
+    public  void main(){
+       
+    }
+
+}   
