332a32fde9b901139faf9ef14cd4592352732715
Backbeatz <connor.pochylko@gmail.com>
2018-12-04 11:59:47 -0700

    Update 8: bug fixes

--- src/java/pkg395project2019/emailSystem.java
+++ src/java/pkg395project2019/emailSystem.java
@@ -27,23 +27,23 @@ public class emailSystem {
      * @return
      */
     public boolean sendUMsg(String targetEmail, String msg1, String msg2 ) {
-                System.out.print("Step1" + targetEmail + msg1 + msg2);
+                
 		final String username = "group395cmptemail@gmail.com";
 		final String password = "RunningOutOfTime1";
-                System.out.print("Step2");
+                
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
-                System.out.print("Step3");
+                
 		Session session = Session.getInstance(props,
 		  new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		  });
-                System.out.print("Step4");
+                
 		try {
 
 			Message message = new MimeMessage(session);
@@ -51,16 +51,16 @@ public class emailSystem {
 			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(targetEmail)); 
 			message.setSubject(msg1);
 			message.setText(msg2);
-                        System.out.print("Step5.1");
+                        
 			Transport.send(message);
-                        System.out.print("Step here?");
+                        
                         
 
 		} catch (MessagingException e) {
-                        System.out.print("Step5.2");
+                        
                         return false;
 		}
-                System.out.print("Step6");
+                
                 return true;
     }
     public  void main(){
